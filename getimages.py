6f6800d9a9843d24eeaceab4264d98907b469eca
Oliver Maerz <7649813+olivermaerz@users.noreply.github.com>
2019-04-14 23:37:06 +0000

    Add script version to get images

---
+++ getimages.py
@@ -0,0 +1,19 @@
+from urllib.request import urlretrieve
+import tarfile
+from io import BytesIO
+import os
+
+
+# loop through the 500 files to download all tar files
+for i in range(0, 499):
+  number_string = str(i).zfill(3)
+  url = 'https://s3.amazonaws.com/google-landmark/train/images_{}.tar'.format(number_string)
+  print("Downloading: {}".format(url))
+  file_tmp = urlretrieve(url, filename=None)[0]
+  # needs to seek in file so open saved temp file
+  tar_ref = tarfile.open(file_tmp)
+  print("Untaring downloaded file")
+  tar_ref.extractall('images/')  
+  tar_ref.close()
+  print('Removing downloaded tar file')
+  os.remove(file_tmp)
